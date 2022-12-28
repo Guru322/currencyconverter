87e55c0539641f3460cc7d516af623f7a961138b
GURU <106463398+Guru322@users.noreply.github.com>
2022-12-28 10:57:53 +0530

    Oops! My bad

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ getButton = document.querySelector("form button");
 for (let i = 0; i < dropList.length; i++) {
     for(let currency_code in country_list){
         // selecting USD by default as FROM currency and INR as TO currency
-        let selected = i == 0 ? currency_code == "IND" ? "selected" : "" : currency_code == "NGN" ? "selected" : "";
+        let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "INR" ? "selected" : "";
         // creating option tag with passing currency code as a text and value
         let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
         // inserting options tag inside select tag
